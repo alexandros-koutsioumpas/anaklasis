fac2f996c4397645f4a228f5e5ddbe120b17bbfb
Alexandros Koutsioumpas <61426965+alexandros-koutsioumpas@users.noreply.github.com>
2021-03-10 21:25:16 +0100

    Add files via upload

--- examples/compare_membrane.py
+++ examples/compare_membrane.py
@@ -12,22 +12,22 @@ patches=[1.0] # single patch 100% covergae
 system = []
 system.append([
 	# Re_sld  Im_sld thk  rough  solv  description 
-	[  2.07e-6, 0.0, 0,    0.89, 0.0,  'Si'],
-	[  3.5e-6,  0.0, 12.6, 3.82, 0.0,  'SiO2'],
-	[  6.15e-6, 0.0, 4.4,  'p0', 1.0,  'D2O'],
-	[  1.7e-6,  0.0, 10.6, 'p0', 0.24, 'heads'],
-	[ -0.4e-6,  0.0, 29.2, 'p0', 0.03, 'tails'],
-	[  1.7e-6,  0.0, 9.0,  'p0', 0.47, 'heads'],
+	[  2.07e-6, 0.0, 0,    1.09, 0.0,  'Si'],
+	[  3.5e-6,  0.0, 12.0, 3.50, 0.0,  'SiO2'],
+	[  6.15e-6, 0.0, 3.6,  'p0', 1.0,  'D2O'],
+	[  1.7e-6,  0.0, 10.5, 'p0', 0.30, 'heads'],
+	[ -0.4e-6,  0.0, 30.9, 'p0', 0.03, 'tails'],
+	[  1.7e-6,  0.0, 6.3,  'p0', 0.20, 'heads'],
 	[  6.15e-6, 0.0,  0,   0.0,  1.0,  'D2O'],
 	])
 
 global_param = [
-	['p0', 2.67, 'roughness'],
+	['p0', 3.6, 'roughness'],
 	]
 
-resolution=[0.1] # dQ/Q=10%
-background = [1.9e-7] # instrumental background
-scale = [0.978] # small scale correction
+resolution=[-1] # pointwise resolution
+background = [5.3e-7] # instrumental background
+scale = [1.05] # small scale correction
 qmax = [0.25] 
 
 res = anaklasis.compare(project, input_file, units, resolution, 
