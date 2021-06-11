50e030d23df67ef88fdb065ddc319da44ecd8781
Alexandros Koutsioumpas <61426965+alexandros-koutsioumpas@users.noreply.github.com>
2021-06-11 17:13:15 +0200

    Delete anaklasis directory

--- anaklasis/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
---------------------------------------------------------------------
-Program ANAKLASIS
-version 1.2, February 2021
-developed by Dr. Alexandros Koutsioumpas. JCNS @ MLZ
-for bugs and requests contact: a.koutsioumpas[at]fz-juelich.de
---------------------------------------------------------------------
-
-"""
