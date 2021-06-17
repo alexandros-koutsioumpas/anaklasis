ec822bcc79516b2a5a8d6c4f91e62c0a9b0e56b2
Alexandros Koutsioumpas <61426965+alexandros-koutsioumpas@users.noreply.github.com>
2021-06-17 13:18:26 +0200

    Update __init__.py

--- anaklasis/__init__.py
+++ anaklasis/__init__.py
@@ -16,6 +16,6 @@ visit project [page](https://github.com/alexandros-koutsioumpas/anaklasis) at *G
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
